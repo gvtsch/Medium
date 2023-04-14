d7ab21ec79e085b7a469f1e61c4225af479a87d1
gvtsch <kempkes.christoph@gmail.com>
2023-04-14 08:29:42 +0200

    Main

--- Neuronales Netz/main.py
+++ Neuronales Netz/main.py
@@ -5,10 +5,6 @@ import matplotlib.pyplot as plt
 # Laden des Datensatzes
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-# Daten betrachten
-plt.imshow(train_images[0])
-plt.show()
-
 # Vorverarbeitung der Trainingsdaten
 train_images = train_images.reshape((-1, 28 * 28))
 train_images = train_images.astype('float32') / 255
